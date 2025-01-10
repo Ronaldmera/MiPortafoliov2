36fa5145e8583f3bb4951ee9cbc75ded6a98b866
Ronaldmera <ronaldalme1230@gmail.com>
2025-01-10 14:47:05 -0500

    skills updat

--- script/transition2.js
+++ script/transition2.js
@@ -7,13 +7,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   
     // Seleccionamos todos los elementos con la clase "skill"
-    const skills = document.querySelectorAll('.skill');
-  
-
-    skills.forEach((skill, index) => {
-      skill.setAttribute('data-aos', 'fade-up'); 
-      skill.setAttribute('data-aos-duration', '500'); // Duración de la animación
-      skill.setAttribute('data-aos-delay', `${index * 50}`); // Retraso progresivo basado en el índice
-    });
+    
   });
   
